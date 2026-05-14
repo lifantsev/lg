c4359c2295040e04df92ead0390294eb45d1dc06
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-05-14 15:16:45 -0700

    lg: remove logging capabilities (leave viewing), because lga is better

--- README.md
+++ README.md
@@ -2,25 +2,29 @@
 
 A simple set of scripts for reading and writing log files.
 
-- [lg](#logging), [lga](#lga), [lge](#lge)
+- [lga & lge](#logging), [lg](#viewing)
 
 ## Usage
 
 ### Logging
 
-To enable logging, `$LGENABLE` must be set to 1 (If set to 0 or unset, `lg` will do nothing and exit successfully). `$LGSTEM` must be set to the name of the program that is logging (this determines which file logs are written to). You may optionally set `$LGSPEC` to a specifier like `ui` or `backend`, to distinguish log lines that are sent to the same file.
+To enable logging, `$LGENABLE` must be set to 1 (If set to 0 or unset, the script will do nothing and exit successfully). `$LGSTEM` must be set to the name of the program that is logging (this determines which file logs are written to). You may optionally set `$LGSPEC` to a specifier like `ui` or `backend`, to distinguish log lines that are sent to the same file. Finally, log lines will be colorized by their log level (first arg).
 
 ``` sh
-lg start
-lg . "quick info update"
-lg F "function_x was called"
-lg I "important info here"
-lg finish
+lga start # the 'a' stands for async (this script does the log writes asynchronously)
+lga . "quick info update"
+lga F "function_x was called" # will print a blue line
+lga I "important info here" # yellow line
+lga R "returned this value" # green line
+lga E "my error" # red line
+lga finish
 ```
 
-The start and finish commands create a much shorter line and thus create visual breaks in the log file. Any other log must have a log level (capital letter in arg 1) and a log string (arg 2). These types of logs include a timestamp in the logfile. There is a special log level `E`, which will cause logs to be written not only to the logfile but also to stdout:
+The start and finish commands create a much shorter line and thus create visual breaks in the log file (and finish lines contain the time delta since the last start line). Any other log must have a log level (capital letter in arg 1) and a log string (arg 2).
+
+To log errors you may use `lga E` but there is also a provided util `lge` that will do the logging and will also print the error message to stdout:
 ``` sh
-lg E "expected A but got B" # warning: this will write to stdout
+lge "expected A but got B" # warning: this will write to stdout
 ```
 
 Logs are written to `$XDG_STATE_HOME/logs/`.
@@ -35,14 +39,6 @@ lg "watch|w" program # uses `watch` to continuously show the last 50 lines
 lg "clear|c" program # clears the log file
 lg "clear|c" all # clears all log files in $XDG_STATE_HOME/logs
 ```
-
-### lga
-
-A secondary script `lga` is provided. The a stands for asynchronous, it forks a background process that completes the logging, which makes scripts faster. It has the same interface as `lg` for [logging](#logging) but does not provide [viewing functionality](#viewing). Also, it never prints to stdout, even if given the 'E' log level.
-
-### lge
-
-A third script `lge` is provided. The e stands for error. Running `lge "my error"` is similar to `lg E "my error"` except it uses [lga](#lga) to write to the logfile, making it slightly faster. It prints an appropriate error message to stdout, unlike [lga](#lga).
 
 ## Installation
 
--- lg.sh
+++ lg.sh
@@ -5,97 +5,44 @@ mkdir -p "$LGPATH"
 arg_action=""
 arg_logname=""
 
-arg_loglevel=""
-arg_logstr=""
-
 function read_logname() {
     if [ -n "${1:-}" ]; then
         arg_logname="$1"
     else
-        echo "lg error: action[$arg_action] or loglevel[$arg_loglevel] expects a log name to act on but none was provided" >> /dev/stderr
+        echo "lg error: action[$arg_action] expects a log name to act on but none was provided" >> /dev/stderr
         exit 1
     fi
 }
 
-function read_logstr() {
-    case "$arg_loglevel" in
-        "start"|"finish") return ;;
-        *) if [ -z "${1:-}" ]; then
-            echo "lg_error: loglevel[$arg_loglevel] requires a logstr but none was passed"
-            exit 1
-        else
-            arg_logstr="$1"
-        fi
-        ;;
-    esac
-}
-
-case "$1" in
+case "${1:-}" in
     "view"|"v")  arg_action="view"  ; read_logname "${2:-}" ;;
     "tail"|"t")  arg_action="tail"  ; read_logname "${2:-}" ;;
     "watch"|"w") arg_action="watch" ; read_logname "${2:-}" ;;
     "clear"|"c") arg_action="clear" ; read_logname "${2:-}" ;;
-    *) 
-        [ -z "${LGENABLE:-}" ] && LGENABLE=0
-        (( ! LGENABLE )) && exit 0 # immediately exit if logging is disabled
-        arg_loglevel="$1" ; read_logstr "${2:-}" ; read_logname "${LGSTEM:-}";;
+    *) arg_action="view" ; read_logname "${1:-}" ;;
 esac
 
 logpath="$LGPATH/$arg_logname.log"
 
-function check_logname() {
-    if [[ "$arg_action" == "clear" ]] && [[ "$arg_logname" == "all" ]]; then
-        return
-    else
-        if ! [[ -f "$logpath" ]]; then
-            if [[ -n "$arg_action" ]]; then
-                echo "lg error: specified logfile[$logpath] does not exist"
-                exit 1
-            else
-                echo -n > "$logpath"
-            fi
-        fi
+if [[ "$arg_action" == "clear" ]] && [[ "$arg_logname" == "all" ]]; then
+    true
+else
+    if ! [[ -f "$logpath" ]]; then
+        echo "lg error: specified logfile[$logpath] does not exist"
+        exit 1
     fi
-}
-check_logname
-
-if [ -n "$arg_action" ]; then
-    case "$arg_action" in
-        "view") less "$logpath" ;;
-        "tail") tail -n 20 "$logpath" ;;
-        "watch") watch -n 0.5 tail -n 50 "$logpath" ;;
-        "clear") 
-            if [[ "$arg_logname" == "all" ]]; then
-                for logfile in "$LGPATH/"*; do echo -n > "$logfile"; done
-            else
-                echo -n > "$logpath"
-            fi
-            ;;
-    esac
-    exit 0
 fi
 
-prefix=""
-[ -n "${LGSPEC:-}" ] && prefix="$LGSPEC: "
-
-case "$arg_loglevel" in
-    "start")
-        echo "${prefix}start $(date +"%S.%3N")" >> "$logpath" 2>/dev/null
-        ;;
-    "finish")
-        tstart="$(cat "$logpath" | grep "^${prefix}start " | sed "s|^${prefix}start ||" | tail -n 1)"
-        tfin="$(cat "$logpath" | grep -v "^${prefix}start \|^${prefix}finish " | tail -n 1 | awk '{ print $4 }')"
-        diff="$(echo "$tfin - $tstart" | bc)"
-        echo "${prefix}finish $diff" >> "$logpath" 2>/dev/null
-        echo >> "$logpath" 2>/dev/null
-        ;;
-    *)
-        if [[ "$arg_logstr" == "-" ]]; then arg_logstr="$(cat)"; fi
-        [[ -z "$arg_logstr" ]] && exit 0
-
-        echo "$arg_loglevel $(date +"%H:%M @ %S.%3N") $arg_loglevel $prefix$arg_logstr" >> "$logpath" 2>/dev/null
-        [ "$arg_loglevel" == E ] && echo "$arg_logname ${prefix}error: $arg_logstr"
+case "$arg_action" in
+    "view") less "$logpath" ;;
+    "tail") tail -n 20 "$logpath" ;;
+    "watch") watch -n 0.5 tail -n 50 "$logpath" ;;
+    "clear") 
+        if [[ "$arg_logname" == "all" ]]; then
+            for logfile in "$LGPATH/"*; do echo -n > "$logfile"; done
+        else
+            echo -n > "$logpath"
+        fi
         ;;
 esac
-
 exit 0
